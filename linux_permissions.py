5016978d3e1c018cd16225ede31ef4b9f2eff646
watari <watari@techwebunraveled.xyz>
2025-12-30 22:52:45 +0300

    new animation

--- linux_permissions.py
+++ linux_permissions.py
@@ -1,2 +1,29 @@
 from manim import *
 
+config.pixel_height = 1080
+config.pixel_width = 1920
+config.frame_height = 8.0
+config.frame_width = 14.222
+
+grid = NumberPlane(
+    x_range=[-config.frame_width / 2, config.frame_width / 2, 1],
+    y_range=[-config.frame_height / 2, config.frame_height / 2, 1],
+    background_line_style={
+        "stroke_color": BLUE_E,
+        "stroke_width": 3,
+        "stroke_opacity": 0.4
+    }
+)
+
+class linux_permissions(Scene):
+    def Permissions(self):
+        intro = Text("""ls -l 
+total 136
+-rw-r--r-- 1 watari watari     46 Dec 30 22:34 README.md""").scale(0.5)
+        self.add(intro)
+
+    def construct(self):
+        self.add(grid, Dot())
+        self.Permissions()
+        
+        
