c23a587e7c2de3a96df678fdf2beb025bc5e8ba9
watari <watari@techwebunraveled.xyz>
2026-01-09 21:10:23 +0300

    restart

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from manim import *
 
 class Pith(Scene):
     def construct(self):
-       # i = 3
-        self.add(grid, Dot())
+       sq = Square(
+        side_length=5, stroke_color=GREEN, fill_opacity=0.75
+        )
+       self.play(Create(sq), run_time = 3)
+       self.wait()
 
-        rect = Rectangle(color=WHITE, height=1, width=1).to_edge(UL)
-        circle = Circle(radius= 1).to_edge(DR)
-        arrow = always_redraw(lambda : Line(start= rect.get_center(), end=circle.get_center()).add_tip())
-        # poly = always_redraw(lambda : Star(color=PURPLE, n=i))
+class Testing(Scene):
+    def construct(self):
+        name = Tex("Sumaiiaa").to_edge(UL, buff=0.5)
+        sq = Square(side_length=0.5).shift(LEFT*3)
+        tri = Triangle().scale(0.6).to_edge(DR)
 
-        self.play(Create(VGroup(rect, circle, arrow)))
-        for i in range(3, 30, 3):
-            self.play(rect.animate.to_edge(UR).scale(2), circle.animate.scale(2).to_edge(DL))
-            self.play(rect.animate.to_edge(UL).scale(0.5), circle.animate.scale(0.5).to_edge(DR))
-        self.wait()
+        self.play(Write(name))
